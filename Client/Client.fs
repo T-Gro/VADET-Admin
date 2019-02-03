491ed0fcd31021f17933bd4572ba4e81294de281
Tomas <tomas.grosup@gmail.com>
2019-02-03 16:21:35 +0100

    Add project files.

---
+++ Client/Client.fs
@@ -0,0 +1,226 @@
+module Client
+
+open Elmish
+open Elmish.React
+
+open Fable.Helpers.React
+open Fable.Helpers.React.Props
+open Fable.PowerPack.Fetch
+
+open Thoth.Json
+
+open Shared
+
+
+open Fulma
+
+open Fable.FontAwesome
+open Fable.FontAwesome.Free
+open Fulma
+
+// The model holds data that you want to keep track of while the application is running
+// in this case, we are keeping track of a counter
+// we mark it as optional, because initially it will not be available from the client
+// the initial value will be requested from server
+type Model = { Counter: Counter option; TableItems : int }
+
+// The Msg type defines what events/actions can occur while the application is running
+// the state of the application changes *only* in reaction to these events
+type Msg =
+| Increment
+| Decrement
+| Delete of int
+| InitialCountLoaded of Result<Counter, exn>
+
+module Server =
+
+    open Shared
+    open Fable.Remoting.Client
+
+    /// A proxy you can use to talk to server directly
+    let api : ICounterApi =
+      Remoting.createApi()
+      |> Remoting.withRouteBuilder Route.builder
+      |> Remoting.buildProxy<ICounterApi>
+
+let initialCounter = Server.api.initialCounter
+
+// defines the initial state and initial command (= side-effect) of the application
+let init () : Model * Cmd<Msg> =
+    let initialModel = { Counter = None; TableItems = 10 }
+    let loadCountCmd =
+        Cmd.ofAsync
+            initialCounter
+            ()
+            (Ok >> InitialCountLoaded)
+            (Error >> InitialCountLoaded)
+    initialModel, loadCountCmd
+
+
+
+// The update function computes the next state of the application based on the current state and the incoming events/messages
+// It can also run side-effects (encoded as commands) like calling the server via Http.
+// these commands in turn, can dispatch messages to which the update function will react.
+let update (msg : Msg) (currentModel : Model) : Model * Cmd<Msg> =
+    match currentModel.Counter, msg with
+    | Some counter, Increment ->
+        let nextModel = { currentModel with Counter = Some { Value = counter.Value + 1 } }
+        nextModel, Cmd.none
+    | Some counter, Decrement ->
+        let nextModel = { currentModel with Counter = Some { Value = counter.Value - 1 } }
+        nextModel, Cmd.none
+    | _, InitialCountLoaded (Ok initialCount)->
+        let nextModel = { Counter = Some initialCount; TableItems = 10 }
+        nextModel, Cmd.none
+    | _, Delete idx ->
+        let nextModel = {currentModel with TableItems = (currentModel.TableItems-1)}
+        nextModel, Cmd.none
+    | _ -> currentModel, Cmd.none
+
+
+
+
+let show = function
+| { Counter = Some counter } -> string counter.Value
+| { Counter = None   } -> "Loading..."
+
+let navBrand =
+    Navbar.navbar [ Navbar.Color IsWhite ]
+        [ Container.container [ ]
+            [ Navbar.Brand.div [ ]
+                [ Navbar.Item.a [ Navbar.Item.CustomClass "brand-text" ]
+                      [ str "e-Shop admin" ] ]
+              Navbar.menu [ ]
+                  [ Navbar.Start.div [ ]
+                      [ Navbar.Item.a [ ]
+                            [ str "Experiment" ]
+                        Navbar.Item.a [ ]
+                            [ str "Paper" ]                       
+                        Navbar.Item.a [ ]
+                            [ str "Results" ] ] ] ] ]
+
+
+let breadcrump =
+    Breadcrumb.breadcrumb [ ]
+        [ Breadcrumb.item [ ]
+              [ a [ ] [ str "e-Shop" ] ]
+          Breadcrumb.item [ ]
+              [ a [ ] [ str "Attributes" ] ]
+          Breadcrumb.item [ ]
+              [ a [ ] [ str "Visual" ] ]
+          Breadcrumb.item [ Breadcrumb.Item.IsActive true ]
+              [ a [ ] [ str "New candidates" ] ] ]
+
+let hero =
+    Hero.hero [ Hero.Color IsInfo               
+                Hero.CustomClass "welcome" ]
+        [ Hero.body [ ]
+            [ Container.container [ Container.IsFluid  ]
+                [ Heading.h1 [ ]
+                      [ str "Hello, Admin." ]
+                   ] ] ]
+
+let info =
+    section [ Class "info-tiles" ]
+        [ Tile.ancestor [ Tile.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ]
+            [ Tile.parent [ ]
+                  [ Tile.child [ ]
+                      [ Box.box' [ ]
+                          [ Heading.p [ ]
+                                [ str "439k" ]
+                            Heading.p [ Heading.IsSubtitle ]
+                                [ str "Users" ] ] ] ]
+              Tile.parent [ ]
+                  [ Tile.child [ ]
+                      [ Box.box' [ ]
+                          [ Heading.p [ ]
+                                [ str "59k" ]
+                            Heading.p [ Heading.IsSubtitle ]
+                                [ str "Products" ] ] ] ]
+              Tile.parent [ ]
+                  [ Tile.child [ ]
+                      [ Box.box' [ ]
+                          [ Heading.p [ ]
+                                [ str "3.4k" ]
+                            Heading.p [ Heading.IsSubtitle ]
+                                [ str "Open Orders" ] ] ] ]
+              Tile.parent [ ]
+                  [ Tile.child [ ]
+                      [ Box.box' [ ]
+                          [ Heading.p [ ]
+                                [ str "19" ]
+                            Heading.p [ Heading.IsSubtitle ]
+                                [ str "Exceptions" ] ] ] ] ] ]
+
+let counter (model : Model) (dispatch : Msg -> unit) =
+    Field.div [ Field.IsGrouped ]
+        [ Control.p [ Control.IsExpanded ]
+            [ Input.text
+                [ Input.Disabled true
+                  Input.Value (show model) ] ]
+          Control.p [ ]
+            [ Button.a
+                [ Button.Color IsInfo
+                  Button.OnClick (fun _ -> dispatch Increment) ]
+                [ str "+" ] ]
+          Control.p [ ]
+            [ Button.a
+                [ Button.Color IsInfo
+                  Button.OnClick (fun _ -> dispatch Decrement) ]
+                [ str "-" ] ] ]
+
+let columns (model : Model) (dispatch : Msg -> unit) =
+            Card.card [ CustomClass "events-card" ]
+                [ Card.header [ ]
+                    [ Card.Header.title [ ]
+                        [ str "New candidates" ]
+                    ]
+                  div [ Class "card-table" ]
+                      [ Content.content [ ]
+                          [ Table.table
+                              [ Table.IsFullWidth
+                                Table.IsStriped ]
+                              [ tbody [ ]
+                                  [ for idx in 1..model.TableItems ->
+                                      tr [ ]
+                                          [ td [ ]
+                                                [ str "Lorem ipsum dolor aire" ]
+                                            td [ ]
+                                                [ Button.a
+                                                    [ Button.Size IsSmall
+                                                      Button.Color IsPrimary
+                                                      Button.OnClick (fun _ -> dispatch (Delete idx)) ]
+                                                    [ str "Action" ] ] ] ] ] ] ]
+                  Card.footer [ ]
+                      [ Card.Footer.div [ ]
+                          [ str "View All" ] ] ]
+          
+            
+
+let view (model : Model) (dispatch : Msg -> unit) =
+    div [ ]
+        [ navBrand
+          Container.container [ Container.IsFluid ]
+              [ Columns.columns [ ]
+                  [ 
+                    Column.column [ Column.Width (Screen.All, Column.Is12) ]
+                      [ breadcrump
+                        hero
+                        //info
+                        columns model dispatch ] ] ] ]
+
+#if DEBUG
+open Elmish.Debug
+open Elmish.HMR
+#endif
+
+Program.mkProgram init update view
+#if DEBUG
+|> Program.withConsoleTrace
+|> Program.withHMR
+#endif
+|> Program.withReact "elmish-app"
+#if DEBUG
+|> Program.withDebugger
+#endif
+|> Program.run
