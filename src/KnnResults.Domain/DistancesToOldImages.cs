59990f88d0d3218a4090ebe5cd66b57123649229
Tomas <tomas.grosup@gmail.com>
2019-11-12 00:53:43 +0100

    new types for dynamic db processing

---
+++ src/KnnResults.Domain/DistancesToOldImages.cs
@@ -0,0 +1,13 @@
+﻿using ProtoBuf;
+
+namespace KnnResults.Domain
+{
+    [ProtoContract]
+    public class DistancesToOldImages
+    {
+        [ProtoMember(1, AsReference = true)]
+        public string OldPatchName { get; set; }
+        [ProtoMember(2)]
+        public float Distance { get; set; }
+    }
+}
---
+++ src/KnnResults.Domain/SimilarityGraph.cs
@@ -0,0 +1,12 @@
+﻿using ProtoBuf;
+using System.Collections.Generic;
+
+namespace KnnResults.Domain
+{
+    [ProtoContract]
+    public class SimilarityGraph
+    {
+        [ProtoMember(1)]
+        public Dictionary<string, DistancesToOldImages[]> ResultsForNewImages { get; set; }
+    }
+}
